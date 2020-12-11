a311062a53a77cf797f2de74620aeba8a5477f14
dyon781 <dusenko@playtika.com>
2020-12-11 12:40:57 +0200

    fix

--- dummy_dag4_repo.py
+++ dummy_dag4_repo.py
@@ -2,4 +2,4 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
 with DAG('dummy_dag4', start_date=datetime(2019, 1, 1)) as dag:
-    op = DummyOperator1(task_id='op')
+    op = DummyOperator(task_id='op')
